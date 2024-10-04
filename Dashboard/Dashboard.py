0b43aea250042cc707079696fd3cbd370825396a
Angela S <angela.sekar@ti.ukdw.ac.id>
2024-10-04 13:58:59 +0700

    Update Dashboard.py

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -6,26 +6,30 @@ import matplotlib.pyplot as plt
 st.title('Dashboard Penyewaan Sepeda')
 st.caption('Angela Sekar Widelia ML-65')
 
-hour = pd.read_csv('hour.csv')  
-hour['dteday'] = pd.to_datetime(hour['dteday'])
-hour['weekday'] = hour['dteday'].dt.weekday
+hour_df = pd.read_csv('hour.csv')  
+hour_df['dteday'] = pd.to_datetime(hour_df['dteday'])
+hour_df['weekday'] = hour_df['dteday'].dt.weekday
 
 # Pertanyaan 1
-by_hour = hour.groupby('hr')['cnt'].sum().reset_index()
+by_hour = hour_df.groupby(by="hr").agg({
+    "cnt": ["sum","mean"],
+    "registered": ["sum","mean"],
+    "casual" : ["sum","mean"]
+}).sort_values(by="hr", ascending=True)
 
 st.header('Jumlah Penyewaan Sepeda pada Setiap Jam')
 
 plt.figure(figsize=(10,6))
 fig, ax = plt.subplots()
-sns.barplot(x='hr', y='cnt', data=hour, palette="pastel")
+sns.barplot(x='hr', y='cnt', data=hour_df, palette="pastel")
 plt.xlabel('Jam dalam 24-hour format')
 plt.ylabel('Jumlah penyewaan')
 st.pyplot(fig)
 
-st.caption('Jam dengan penyewaan tertinggi adalah jam 8 pagi, dengan jumlah penyewaan mencapai 839.')
+st.caption('Jam dengan penyewaan tertinggi adalah jam 17.00, dengan jumlah penyewaan mencapai 336860.')
 
 # Pertanyaan 2
-by_day = hour.groupby('weekday')['cnt'].sum().reset_index()
+by_day = hour_df.groupby('weekday')['cnt'].sum().reset_index()
 by_day['weekday'] = by_day['weekday'].replace({
     0: 'Sun',
     1: 'Mon',
